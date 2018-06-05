a579766aba3b415041eae8927515d70c9f01f88b
WINDOWS 8.1 <fabrizio.07.ff@gmail.com>
2018-06-05 08:34:33 -0400

    Potencia

--- src/main/java/OperacionesArtimeticas.java
+++ src/main/java/OperacionesArtimeticas.java
@@ -3,4 +3,8 @@ public class OperacionesArtimeticas {
     public int Suma(int a, int b){
         return a+b;
     }
+
+    public static int Potencia(int a,int b){
+        return (int)Math.pow(a, b);
+    }
 }
--- src/test/java/OperacionesArtimeticasTest.java
+++ src/test/java/OperacionesArtimeticasTest.java
@@ -1,4 +1,14 @@
-public class OperacionesArtimeticasTest {
+import junit.framework.Assert;
+import org.junit.Test;
 
+public class OperacionesArtimeticasTest {
+    @Test
+    public void verificarPotencia() {
+        int a=2;
+        int b=2;
+        int expectedResults= 4;
+        int actualResults = OperacionesArtimeticas.Potencia(a,b);
+        Assert.assertEquals(expectedResults,actualResults);
+    }
 
 }
