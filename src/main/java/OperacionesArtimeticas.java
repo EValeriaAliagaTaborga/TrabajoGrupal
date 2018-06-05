b5f2325788d898af5d732f9376269c3587d68526
arceus6666 <arceus6666@hotmail.com>
2018-06-05 08:25:17 -0400

    commit div

--- src/main/java/OperacionesArtimeticas.java
+++ src/main/java/OperacionesArtimeticas.java
@@ -3,4 +3,17 @@ public class OperacionesArtimeticas {
     public int Suma(int a, int b){
         return a+b;
     }
+
+    public int div(int a, int b){
+        if (a > Integer.MAX_VALUE || b > Integer.MAX_VALUE){
+            System.out.println("valores muy altos");
+            return 0;
+        } else if (b == 0){
+            System.out.println("división por 0");
+            return 0;
+        }else {
+            return a / b;
+        }
+        
+    }
 }
