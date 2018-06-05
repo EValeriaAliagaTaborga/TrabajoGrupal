8cbd88fb4bdf3492f70cd27f1d8f5d717b35257f
Vale Succi <valesuc98@gmail.com>
2018-06-05 08:37:20 -0400

    Adicionando multiplicación

--- src/main/java/OperacionesArtimeticas.java
+++ src/main/java/OperacionesArtimeticas.java
@@ -3,4 +3,9 @@ public class OperacionesArtimeticas {
     public int Suma(int a, int b){
         return a+b;
     }
+
+    public int multiplicar(int a, int b) {
+        return a*b;
+    }
+
 }
--- src/test/java/OperacionesArtimeticasTest.java
+++ src/test/java/OperacionesArtimeticasTest.java
@@ -1,4 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class OperacionesArtimeticasTest {
 
+    @Test
+    public void verificarMultiplicacion(){
+        int expectedResult = 140;
+
+        int actualResult = (new OperacionesArtimeticas()).multiplicar(28,5);
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
 
 }
